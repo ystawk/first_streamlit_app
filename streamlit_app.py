92c90c1270b111f9765cc591bed6868d3b68729d
ystawk <56367037+ystawk@users.noreply.github.com>
2023-01-13 15:15:20 -0500

    Update streamlit_app.py
    added one menu item and imojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')

345913aea4c5846e65f8f13abedabda61c289748
ystawk <56367037+ystawk@users.noreply.github.com>
2023-03-03 13:26:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("Hello from Snowflake:")
 streamlit.dataframe(my_data_rows)
+
+## Allow the user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('Thank you for adding a new fruit', add_my_fruit)
